bc61b9d73679a9680554a8b2e3a3d227008fbd70
Mario Mendiola Jr <mariomendiolajr@Marios-MacBook-Pro-2.local>
2024-07-12 10:28:14 -0700

    Admin homepage created and cypressio tests created

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -29,17 +29,16 @@ onMounted(async () => {
 
 async function getUser() {
   localStorageUser.value = JSON.parse(localStorage.getItem("user"));
-  if (localStorageUser.value !== null && localStorageUser.value.id !== null)
-  {
+  if (localStorageUser.value !== null && localStorageUser.value.id !== null) {
     await UserServices.getUserById(localStorageUser.value.id)
-    .then((response) => {
-      users.value = response.data;
-    })
-    .catch((error) => {
-      snackbar.value.value = true;
-      snackbar.value.color = "error";
-      snackbar.value.text = error.response.data.message;
-    });
+      .then((response) => {
+        users.value = response.data;
+      })
+      .catch((error) => {
+        snackbar.value.value = true;
+        snackbar.value.color = "error";
+        snackbar.value.text = error.response.data.message;
+      });
   }
 }
 
@@ -90,78 +89,53 @@ function closeEdit() {
     </div>
   </v-container>
   <v-card class="mx-auto" max-width="1000" tile>
-        <v-avatar :rounded="5" size="150">
-            <v-img
-                :width="800"
-                aspect-ratio="16/9"
-                src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-            ></v-img>        </v-avatar>
-        <v-col cols="15" class="d-flex justify-end">
-          <!-- Pass the function with the users information to the openEdit function -->
-            <v-icon size="small" icon="mdi-pencil" @click.stop="openEdit(users)"></v-icon>
-        </v-col>
-      <v-table class="rounded-lg elevation-5">
-        <thead>
-          <tr>
-            <th class="text-left">Name</th>
-            <th class="text-left">Role</th>
-            <th class="text-left">ID</th>
-          </tr>
-        </thead>
-        <tbody>
-            <tr>
-              <!-- Display the users information in a table -->
-            <td>{{ users.firstName }} {{ users.lastName }}</td>
-            <td>{{ users.email }}</td>
-            <td>{{ users.id }}</td>
-          </tr>
-        </tbody>
-      </v-table>
-    </v-card>
+    <v-avatar :rounded="5" size="150">
+      <v-img :width="800" aspect-ratio="16/9" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-img>
+    </v-avatar>
+    <v-col cols="15" class="d-flex justify-end">
+      <!-- Pass the function with the users information to the openEdit function -->
+      <v-icon size="small" icon="mdi-pencil" @click.stop="openEdit(users)"></v-icon>
+    </v-col>
+    <v-table class="rounded-lg elevation-5">
+      <thead>
+        <tr>
+          <th class="text-left">Name</th>
+          <th class="text-left">Role</th>
+          <th class="text-left">ID</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <!-- Display the users information in a table -->
+          <td>{{ users.firstName }} {{ users.lastName }}</td>
+          <td>{{ users.role }}</td>
+          <td>{{ users.id }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-card>
 
-    <v-dialog persistent :model-value="isEdit" width="800">
-      <v-card class="rounded-lg elevation-5">
-        <v-card-item>
-          <v-card-title class="headline mb-2">
-            {{ isEdit ? "Edit User Profile" : "" }}
-          </v-card-title>
-        </v-card-item>
-        <v-card-text>
-            <v-text-field
-              v-model="newUser.firstName"
-              label="First name"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="newUser.lastName"
-              label="Last name"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="newUser.email"
-              label="Email"
-              required
-            ></v-text-field>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              variant="flat"
-              color="secondary"
-              @click="isEdit ? closeEdit() : false"
-              >Close</v-btn
-            >
-            <v-btn
-              variant="flat"
-              color="primary"
-              @click="
-                updateUser()
-              "
-              >{{
+  <v-dialog persistent :model-value="isEdit" width="800">
+    <v-card class="rounded-lg elevation-5">
+      <v-card-item>
+        <v-card-title class="headline mb-2">
+          {{ isEdit ? "Edit User Profile" : "" }}
+        </v-card-title>
+      </v-card-item>
+      <v-card-text>
+        <v-text-field v-model="newUser.firstName" label="First name" required></v-text-field>
+        <v-text-field v-model="newUser.lastName" label="Last name" required></v-text-field>
+        <v-text-field v-model="newUser.email" label="Email" required></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn variant="flat" color="secondary" @click="isEdit ? closeEdit() : false">Close</v-btn>
+        <v-btn variant="flat" color="primary" @click="
+          updateUser()
+          ">{{
                 isEdit ? "Update User Profile" : ""
-              }}</v-btn
-            >
-          </v-card-actions>
-      </v-card>
-    </v-dialog>
+              }}</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
