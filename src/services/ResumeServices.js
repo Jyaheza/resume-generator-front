d8fd5496cdd7808fda85843d2e9b5b81a040ea43
Mark <sandmdelgados@gmail.com>
2024-07-22 07:35:11 -0500

    Set student landing page up with resume cards that are able to view the resume pdf, delete the resume, and I stubbed in some job match score ui.

--- src/services/ResumeServices.js
+++ src/services/ResumeServices.js
@@ -4,11 +4,17 @@ export default {
   getResumes() {
     return apiClient.get("resumes");
   },
+  getResumesMeta(userId){
+    return apiClient.get(`resumes/meta/user/${userId}`);
+  },
   getResumesByUserId(userId) {
     return apiClient.get(`resumes/user/${userId}`);
   },
   getResume(id) {
     return apiClient.get(`resumes/${id}`);
+  },
+  getResumePdf(id){
+    return apiClient.get(`resumes/pdf/${id}`)
   },
   addResume(resumeParams) {
     return apiClient.post(`resumes`, resumeParams);
